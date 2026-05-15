a36140d04783355c39e1900fb028507d3cd10438
Hoa Vu <hvutrong@gmail.com>
2026-05-15 04:56:02 -0700

    Homemade SPSA script -------------------------------------------------- Results of DIPUTEXP vs Diputs (15+0.15, NULL, 64MB, UHO_Lichess_4852_v1.epd): Elo: 16.12 +/- 12.10, nElo: 24.28 +/- 18.19 LOS: 99.56 %, DrawRatio: 44.08 %, PairsRatio: 1.32 Games: 1402, Wins: 442, Losses: 377, Draws: 583, Points: 733.5 (52.32 %) Ptnml(0-2): [33, 136, 309, 179, 44], WL/DD Ratio: 1.31 LLR: 2.22 (101.1%) (-2.20, 2.20) [0.00, 10.00] --------------------------------------------------

--- src/search.jl
+++ src/search.jl
@@ -534,7 +534,7 @@ function negamax(
 
     # Reverse futility pruning
     if depth ≤ 7 && !in_check && !is_pv_node && !is_singular
-        if eval ≥ β + 175 * depth - 75 * improving
+        if eval ≥ β + 175 * depth - 85 * improving
             if tt_best == Move(0) || history[stm, from(tt_best).val, to(tt_best).val, tid] > 7000
                 return (eval + β) ÷ 2
             end
@@ -602,7 +602,7 @@ function negamax(
         # SEE pruning + cache for LMR tweak below.
         see_val = (!in_check && !is_pv_node && !is_singular) ? see(b, m) : 0
         if depth ≤ 8 && !in_check && !is_pv_node && !is_singular
-            see_threshold = is_capture ? -30 * depth : -80 * depth * depth
+            see_threshold = is_capture ? -40 * depth : -80 * depth * depth
             see_val < see_threshold && continue
         end
 
