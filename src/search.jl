b5e2e5d2904d6b2f2ce383dcc1405d206ed7f2ea
Hoa Vu <hvutrong@gmail.com>
2026-05-16 18:40:12 -0700

    Add triple extension -------------------------------------------------- Results of DIPUTEXP vs Diputs (15+0.15, NULL, 64MB, UHO_Lichess_4852_v1.epd): Elo: 23.07 +/- 14.93, nElo: 35.38 +/- 22.83 LOS: 99.88 %, DrawRatio: 41.12 %, PairsRatio: 1.38 Games: 890, Wins: 275, Losses: 216, Draws: 399, Points: 474.5 (53.31 %) Ptnml(0-2): [12, 98, 183, 123, 29], WL/DD Ratio: 1.06 LLR: 2.23 (101.5%) (-2.20, 2.20) [0.00, 10.00] --------------------------------------------------

--- src/search.jl
+++ src/search.jl
@@ -619,8 +619,11 @@ function negamax(
                                  ply, key_history, tid; excluded_move = m)
             if sing_score < singular_β
                 ext = 1
-                if sing_score < singular_β - 40 
+                if sing_score < singular_β - 20 
                     ext += 1 # double extension
+                end
+                if sing_score < singular_β - 40 
+                    ext += 1 # triple extension
                 end
             elseif singular_β ≥ β
                 return singular_β  # multicut
